d74ed6f9f7decf74deaa7ed6dfd06179e954bf99
elpopes <lorenzotijerina@gmail.com>
2024-04-23 17:16:12 -0400

    add breath widget

---
+++ lib/widgets/breath_ball.dart
@@ -0,0 +1,62 @@
+import 'dart:async';
+import 'package:flutter/material.dart';
+
+class BreathBall extends StatefulWidget {
+  @override
+  _BreathBallState createState() => _BreathBallState();
+}
+
+class _BreathBallState extends State<BreathBall> with SingleTickerProviderStateMixin {
+  AnimationController _controller;
+  Animation<double> _animation;
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = AnimationController(
+      duration: const Duration(seconds: 4), // Duration of one breath cycle
+      vsync: this,
+    );
+
+    _animation = Tween<double>(begin: 100.0, end: 200.0).animate(_controller)
+      ..addListener(() {
+        setState(() {});
+      })
+      ..addStatusListener((status) {
+        if (status == AnimationStatus.completed) {
+          _controller.reverse();
+        } else if (status == AnimationStatus.dismissed) {
+          _controller.forward();
+        }
+      });
+
+    _controller.forward();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Container(
+        width: _animation.value,
+        height: _animation.value,
+        decoration: BoxDecoration(
+          color: Colors.blueAccent,
+          shape: BoxShape.circle,
+        ),
+        child: Center(
+          child: Text(
+            'Breathe in... Breathe out...',
+            textAlign: TextAlign.center,
+            style: TextStyle(color: Colors.white),
+          ),
+        ),
+      ),
+    );
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+}
